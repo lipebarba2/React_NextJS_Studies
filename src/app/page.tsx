ecae25467d458f8a5355e93359b69f2c18aafd2f
lipebarba2 <felipecinemkt@gmail.com>
2024-09-12 07:19:44 -0300

    first

---
+++ src/app/page.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import { Users } from "./types/User";
+
+export default function Home() {
+
+  const [users, setUsers] = useState<Users[]>([])
+  const [loading, setLoading] = useState(true)
+
+  const getUsers = async () => {
+    setLoading(true)
+    try {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users')
+      const json = await res.json()
+
+    setUsers(json)
+    } catch (err) {
+      console.log("Deu algo errado")
+    }
+    setLoading(false)
+  }
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  const handleAddNewPost = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: 'POST', 
+      headers: {
+        'Conten-type': 'application/json; charset=UTF-8'
+      },
+      body: JSON.stringify({
+        title: 'Post de teste',
+        body: 'Corpo de teste',
+        userId: 99
+      })
+    });
+    const json = await res.json()
+
+    console.log(json)
+  }
+
+  return (
+    <div className="container mx-auto">
+      <button onClick={handleAddNewPost}>Adicionar Novo Post</button>
+      <h1 className="text-3xl">Lista de Usuários</h1>
+
+      {loading && "Carregando .."}
+      {!loading && users.length > 0 &&
+      <ul>
+
+        {users.map((item) => (
+          <li key={item.id}>{item.name} - {(item.address.city)}</li>
+        ))}
+       
+      </ul>
+      }
+       {!loading && users.length === 0 && "Não há usuários para exibir"}
+      
+    </div>
+  );
+}
---
+++ src/app/types/User.ts
@@ -0,0 +1,8 @@
+export type Users = {
+    id: number,
+    name: string,
+    email: string,
+    address: {
+        city: string,
+    }
+}
